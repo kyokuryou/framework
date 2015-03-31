47aa5a2bd8c589e2db7ac37d22a375c7a63f71e3
kyokuryou <kyokuryou@live.com>
2015-03-31 16:28:08 +0800

    修改:springmvc拦截器

---
+++ framework_src/src/org/core/support/web/LoginVerifyInterceptor.java
@@ -0,0 +1,30 @@
+package org.core.support.web;
+
+import org.core.bean.SystemConfig;
+import org.core.utils.SystemConfigUtil;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 判断会员是否登录
+ */
+public class LoginVerifyInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        SystemConfig systemConfig = SystemConfigUtil.getSystemConfig();
+        String loginMemberId = (String)  request.getSession().getAttribute(systemConfig.getLoginSessionId());
+        if (loginMemberId == null) {
+            Cookie cookie = new Cookie(systemConfig.getLoginCookieName(), null);
+            cookie.setPath(request.getContextPath());
+            cookie.setMaxAge(0);
+            response.addCookie(cookie);
+            response.sendRedirect("account/login");
+            return false;
+        }
+        return true;
+    }
+}
